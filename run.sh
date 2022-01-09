dc66300599a00d45277a0a2b08a3fc8fd115aed4
oyewunmio <oluwaseyioyewunmi99@gmail.com>
2022-01-09 18:53:27 +0100

    changed from sh to python

--- run.sh
+++
@@ -1,7 +0,0 @@
-export FN_AUTH_REDIRECT_URI=http://127.0.0.1:8080/hordanso-google/auth
-# export FN_BASE_URI=http://127.0.0.1:8080/
-export FLASK_APP=app.py
-export FLASK_DEBUG=TRUE
-export FN_FLASK_SECRET_KEY=hordanso_web
-
-python -m flask run -p 8080
